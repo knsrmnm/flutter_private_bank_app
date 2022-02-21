8f3a0f39e51a9e8535f0c8a042b8296a85bec9f8
Ken Minami <kenminami.dev@gmail.com>
2022-02-22 08:12:30 +0900

    Add home screen.

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// Home screen
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
